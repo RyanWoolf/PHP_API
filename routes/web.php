5aae6f739262d3fe79a457fb9e879f7cddc6e47d
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-03-09 14:45:24 +1000

    First view page added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route that redirects to a view page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// Route to users - string
+Route::get('/users', function () {
+    return '<h1>Welcome to the users page</h1>';
+});
+
+// 다른 API 와 같이 같은 라우트 주소가 있을때 가장 마지막에 선언된 것으로 연결함.
+// Route to users - Array(제이손)
+Route::get('/users', function () {
+    return ['PHP', 'HTML', 'LARAVEL'];
+});
+
+// Route to users - 제이손 오브젝트
+Route::get('/users', function () {
+    return response()->json([
+        'name' => 'Ryan',
+        'course' => 'Laravel course for beginners'
+    ]);
+});
+
+// Route to users - 함수
+Route::get('/users', function () {
+    return redirect('/');
 });
